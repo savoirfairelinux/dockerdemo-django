34dae472d4fd7190bde734a47c459ea0a8ccdfaa
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2017-01-20 15:51:08 -0500

    Added hostname in the base header

--- demo_project/context_processors.py
+++ demo_project/context_processors.py
@@ -1,7 +1,13 @@
+import socket
+
 from redis import Redis
 
 
 redis = Redis(host='cache', port=6379)
+
+
+def hostname(request):
+    return {'hostname': socket.gethostname(), }
 
 
 def views_counter(request):
